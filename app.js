4dd44aa62faddabe44a63fff3c8623d05c745a15
Md Jishan Ansari <86798430+md-jishan-ansari@users.noreply.github.com>
2021-11-01 23:14:18 +0530

    app modified

--- app.js
+++ app.js
@@ -34,7 +34,7 @@ app.use(cors());
 // Serving static files
 
 // Set security HTTP headers
-app.use(helmet());
+// app.use(helmet());
 
 // Development logging
 if (process.env.NODE_ENV === 'development') {
@@ -42,35 +42,35 @@ if (process.env.NODE_ENV === 'development') {
 }
 
 // Limit requests from same API
-const limiter = rateLimit({
-    max: 100,
-    windowMs: 60 * 60 * 1000,
-    message: 'Too many requests from this IP, please try again in an hour!'
-});
-app.use('/api', limiter);
+// const limiter = rateLimit({
+//     max: 100,
+//     windowMs: 60 * 60 * 1000,
+//     message: 'Too many requests from this IP, please try again in an hour!'
+// });
+// app.use('/api', limiter);
 
 // Body parser, reading data from body into req.body
-app.use(cookieParser());
+// app.use(cookieParser());
 
 // Data sanitization against NoSQL query injection
-app.use(mongoSanitize());
+// app.use(mongoSanitize());
 
 // Data sanitization against XSS
-app.use(xss());
+// app.use(xss());
 
 // Prevent parameter pollution
-app.use(
-    hpp({
-        whitelist: [
-            'duration',
-            'ratingsQuantity',
-            'ratingsAverage',
-            'maxGroupSize',
-            'difficulty',
-            'price'
-        ]
-    })
-);
+// app.use(
+//     hpp({
+//         whitelist: [
+//             'duration',
+//             'ratingsQuantity',
+//             'ratingsAverage',
+//             'maxGroupSize',
+//             'difficulty',
+//             'price'
+//         ]
+//     })
+// );
 
 // Test middleware
 app.use((req, res, next) => {
